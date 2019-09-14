06631b6473dae69304235353d4c27d524618a385
CortexCucurrit <danielcorkran@gmail.com>
2019-09-14 01:18:53 -0400

    added password protection

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,72 @@
+$("#btnLogin").click(function (event) {
+    formHandler(event);
+});
+
+function toggleDisplay(item) {
+    var x = document.getElementById(item);
+    if (x.style.display === 'none') {
+        x.style.display = 'block';
+    } else {
+        x.style.display = 'none';
+    }
+}
+
+function formHandler(event) {
+    event.preventDefault();
+    var credential = $("#inputPassword").val();
+    switch (credential) {
+        case "CarriageHouse":
+            toggleDisplay('topNav');
+            toggleDisplay('navPhotos');
+            toggleDisplay('navRegistry');
+            toggleDisplay('join-us');
+            toggleDisplay('events');
+            toggleDisplay('accommodations');
+            toggleDisplay('travel');
+            toggleDisplay('things-to-do');
+            toggleDisplay('rsvp-container');
+            toggleDisplay('rehearsal');
+            toggleDisplay('ceremony');
+            toggleDisplay('login');
+            break;
+        case "Westover":
+            toggleDisplay('topNav');
+            toggleDisplay('join-us');
+            toggleDisplay('events');
+            toggleDisplay('accommodations');
+            toggleDisplay('travel');
+            toggleDisplay('things-to-do');
+            toggleDisplay('rsvp-container');
+            toggleDisplay('rehearsal');
+            toggleDisplay('ceremony');
+            toggleDisplay('login');
+            break;
+        case "SealHarbor":
+            toggleDisplay('topNav');
+            toggleDisplay('join-us');
+            toggleDisplay('events');
+            toggleDisplay('accommodations');
+            toggleDisplay('travel');
+            toggleDisplay('things-to-do');
+            toggleDisplay('rsvp-container');
+            toggleDisplay('ceremony');
+            toggleDisplay('login');
+            break;
+        case "BarHarbor":
+            toggleDisplay('topNav');
+            toggleDisplay('join-us');
+            toggleDisplay('events');
+            toggleDisplay('accommodations');
+            toggleDisplay('travel');
+            toggleDisplay('things-to-do');
+            toggleDisplay('rsvp-container');
+            toggleDisplay('login');
+            break;
+        default:
+            console.log("Credential is invalid");
+    }
+}
+
 function scroll_at(clicked_link, nav_height) {
     var element_id = clicked_link.attr('href');
     console.log(element_id);
@@ -15,6 +84,9 @@ function scroll_at(clicked_link, nav_height) {
     }
 }
 
+function openWindow(url) {
+    window.open(url, '_blank');
+}
 
 jQuery(document).ready(function () {
 	/* Navigation */
@@ -27,6 +99,3 @@ jQuery(document).ready(function () {
     });
 });
 
-function openWindow(url) {
-    window.open(url, '_blank');
-}
